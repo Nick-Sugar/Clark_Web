4a558ad6aca3df69bb5cb7014d968a7754199845
UserIsDevious <user.devious@gmail.com>
2020-08-22 23:20:32 +0900

    test_js_006

---
+++ Scripts/js/index.js
@@ -0,0 +1,14 @@
+firebase.auth().onAuthStateChanged( (user) => {
+      let h1   = document.querySelector('h1');
+      let info = document.querySelector('#info');
+
+      if(user) {
+        h1.innerText   = 'Login Complete!';
+        info.innerHTML = `${user.displayName}さんがログインしました<br>` +
+                         `(${user.uid})`;
+        console.log(user);
+      }
+      else {
+        h1.innerText = 'Not Login';
+      }
+    });
