139a21ab001c939dd0dc96c36ef2f8486eddd194
UserIsDevious <user.devious@gmail.com>
2020-08-26 00:47:33 +0900

    test

--- Scripts/js/join.js
+++ Scripts/js/join.js
@@ -1,4 +1,4 @@
- let databaseRoot = 'myapp/multi/';
+let databaseRoot = 'clark-bf1df/';
     let key = database.ref(databaseRoot + room + '/_join_').push({ joined : 'unknown'}).key
     clientId = 'member_' + key;
     database.ref(databaseRoot + room + '/_join_/' + key).update({ joined : clientId});
