eb64f59ca0b9b0270aff5c296cddec51458b63db
nick-sugar <nicksugar0141@gmail.com>
2020-09-09 16:28:16 +0900

    addcsslink

--- Scripts/js/test.js
+++ Scripts/js/test.js
@@ -1,17 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
+    let localVideo = document.getElementById('local_video');
+});
 
 function startVideo() {
+    let localStream;
     navigator.mediaDevices.getUserMedia(
         { video: true, audio: true },
         function (stream) { localStream = stream; },
         function (error) {
             alert("メディアが取得できません");
         });
-
-}
-
-let peer = new RTCPeerConnection({ iceServers: [{ "url": "stun:stun.l.google.com:19302" }] });
+    let peer = new RTCPeerConnection({ iceServers: [{ "url": "stun:stun.l.google.com:19302" }] });
 
     peer.AddStream(localStream);
 
-});
+}
