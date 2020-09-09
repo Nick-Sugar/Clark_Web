c46de45b5f47e2ea156979c6cb5f474f963cfedb
nick-sugar <nicksugar0141@gmail.com>
2020-09-09 17:17:40 +0900

    addwindous

--- Scripts/js/test.js
+++ Scripts/js/test.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-    let localVideo = document.getElementById('local_video');
+    
 });
 
 function startVideo() {
@@ -10,8 +10,20 @@ function startVideo() {
         function (error) {
             alert("メディアが取得できません");
         });
-    let peer = new RTCPeerConnection({ iceServers: [{ "url": "stun:stun.l.google.com:19302" }] });
+    let peer = new RTCPeerConnection({ iceServers: [{ "urls": "stun:stun.l.google.com:19302" }] });
 
-    peer.AddStream(localStream);
+    peer.addStream(localStream);
 
+}
+async function startWindow() {
+    try {
+        let mediaStream = await navigator.mediaDevices.getDisplayMedia({ video: true });
+        //videoElement.srcObject = mediaStream;
+
+        let localVideo = document.getElementById('local_video');
+//        localVideo.src = window.URL.createObjectURL(mediaStream);
+        localVideo.srcObject = mediaStream;
+    } catch (e) {
+        console.log('Unable to acquire screen capture: ' + e);
+    }
 }
