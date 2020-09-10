50babefce2f32bceabe226b10532b5d2d67e4202
nick-sugar <nicksugar0141@gmail.com>
2020-09-10 09:34:49 +0900

    競合修正

--- Scripts/js/test.js
+++ Scripts/js/test.js
@@ -14,31 +14,24 @@ function startVideo() {
         { video: true, audio: true },
         function (stream) {
             peer.addStream(stream);
+            View_Monitor(stream);
         },
         function (error) {
             alert("メディアが取得できません");
         });
     let peer = new RTCPeerConnection({ iceServers: [{ "urls": "stun:stun.l.google.com:19302" }] });
-
-<<<<<<< HEAD
-=======
     // peer.addStream(localStream);
-    View_Monitor(localStream)
+    
 
->>>>>>> 4e93f9c88ef5475efb6bd7d467b06439cce98430
 }
 async function startWindow() {
     try {
         let mediaStream = await navigator.mediaDevices.getDisplayMedia({ video: true });
         //videoElement.srcObject = mediaStream;
 
-<<<<<<< HEAD
         let localVideo = document.getElementById('local_video');
         localVideo.src = window.URL.createObjectURL(mediaStream);
-=======
         
-//        localVideo.src = window.URL.createObjectURL(mediaStream);
->>>>>>> 4e93f9c88ef5475efb6bd7d467b06439cce98430
         localVideo.srcObject = mediaStream;
     } catch (e) {
         console.log('Unable to acquire screen capture: ' + e);
